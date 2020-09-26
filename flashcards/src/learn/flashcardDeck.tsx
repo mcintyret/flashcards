99f9740e7173b5c9c4e13b421c3d783e4a65a1e2
tmcintyre <tmcintyre@palantir.com>
2020-09-26 22:29:04 +0100

    scroll cards with arrow keys

--- flashcards/src/learn/flashcardDeck.tsx
+++ flashcards/src/learn/flashcardDeck.tsx
@@ -9,19 +9,36 @@ export interface IFlashcardDeckProps {
 export const FlashcardDeck: React.FC<IFlashcardDeckProps> = ({ flashcards }) => {
     const [index, setIndex] = React.useState(0);
 
+    const moveLeft = () => setIndex(Math.max(0, index - 1));
+    const moveRight = () => setIndex(Math.min(index + 1, flashcards.length - 1));
+
+    const keyListener = (evt: KeyboardEvent) => {
+        if (evt.key === "ArrowLeft") {
+            moveLeft();
+        } else if (evt.key === "ArrowRight") {
+            moveRight();
+        }
+    }
+
+    React.useEffect(() => {
+        window.addEventListener("keydown", keyListener);
+        return () => {
+            window.removeEventListener("keydown", keyListener);
+        }
+    });
+
     const leftButton = (
-        <button className="deck-button" disabled={index === 0} onClick={() => setIndex(index - 1)}>{"<"}</button>
+        <button className="deck-button" disabled={index === 0} onClick={moveLeft}>{"<"}</button>
     );
     const rightButton = (
-        <button className="deck-button" disabled={index >= flashcards.length - 1}
-                onClick={() => setIndex(index + 1)}>{">"}</button>
+        <button className="deck-button" disabled={index >= flashcards.length - 1} onClick={moveRight}>{">"}</button>
     );
 
-    return(
-        <div className="flashcard-deck">
+    return (
+        <div className="flashcard-deck" onKeyDown={keyListener}>
             {leftButton}
             <Flashcard2 {...flashcards[index]}/>
             {rightButton}
         </div>
-    )
+    );
 };
